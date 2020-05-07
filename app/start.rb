45acd35e81b80304edcf10f9b8aa1e210b2ca198
github username <github email address>
2020-05-07 14:10:49 -0500

    Forest Branch completed

--- app/start.rb
+++ app/start.rb
@@ -21,7 +21,7 @@ argue = Choicea.all[1].description
 apologize = Choiceb.all[1].description
 
 exclaim = Choicea.all[3].description
-lower_wep = Choiceb.all[3].description
+lower_weputs = Choiceb.all[3].description
 
 decline = Choicea.all[2].description
 accept = Choiceb.all[2].description
@@ -30,7 +30,7 @@ maybe_not = Choicea.all[4].description
 maybe_so = Choiceb.all[4].description
 
 route1 = [forest,town]
-cold_grasp = [run,fight]
+cold_grasputs = [run,fight]
 townintro = [inspect,explore]
 dragon = [hide,fight]
 fight1 = [tail,wing]
@@ -41,28 +41,28 @@ ugly = [exclaim, lower_wep]
 offer = [decline, accept]
 unbefriend = [maybe_not, maybe_so]
 
-prompt.ask("What is your name young adventurer?", required: true)
+# prompt.ask("What is your name young adventurer?", required: true)
 
 if prompt.select(beginning_message, route1) == forest
     if prompt.select(Outcome.all[0].outcome, cold_grasp) == run
        if  prompt.select(Outcome.all[1].outcome, bandits) == argue
-        p Outcome.all[2].outcome
+        puts Outcome.all[2].outcome
        else 
         if prompt.select(Outcome.all[3].outcome, offer) == decline
-            p Outcome.all[4].outcome
+            puts Outcome.all[4].outcome
         else
-            p Outcome.all[5].outcome
+            puts Outcome.all[5].outcome
         end
        end
 # beginning of stand and fight
     else 
         if prompt.select(Outcome.all[6].outcome, ugly) == exclaim
-            p Outcome.all[7].outcome
+            puts Outcome.all[7].outcome
         else
             if prompt.select(Outcome.all[8].outcome, unbefriend) == maybe_not
-                p Outcome.all[9].outcome
+                puts Outcome.all[9].outcome
             else
-                p Outcome.all[10].outcome
+                puts Outcome.all[10].outcome
             end
         end
     end
@@ -70,72 +70,72 @@ if prompt.select(beginning_message, route1) == forest
 else 
     if prompt.select(Outcome.all[11].outcome, townintro) == inspect
         if prompt.select(Outcome.all[12].outcome, dragon) == hide
-            p Outcome.all[14].outcome
+            puts Outcome.all[14].outcome
             if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                p Outcome.all[19].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[19].outcome
+                puts Outcome.all[21].outcome
             else
-            p Outcome.all[20].outcome
-            p Outcome.all[21].outcome
+            puts Outcome.all[20].outcome
+            puts Outcome.all[21].outcome
             end
             
     
         else
             if prompt.select(Outcome.all[15].outcome,fight1) == tail
-                p Outcome.all[16].outcome
+                puts Outcome.all[16].outcome
                 
                 if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                    p Outcome.all[19].outcome
-                    p Outcome.all[21].outcome
+                    puts Outcome.all[19].outcome
+                    puts Outcome.all[21].outcome
                 else
-                p Outcome.all[20].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[20].outcome
+                puts Outcome.all[21].outcome
                 end
             
             
             else
-                p Outcome.all[17].outcome
+                puts Outcome.all[17].outcome
                 
                 if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                    p Outcome.all[19].outcome
-                    p Outcome.all[21].outcome
+                    puts Outcome.all[19].outcome
+                    puts Outcome.all[21].outcome
                 else
-                p Outcome.all[20].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[20].outcome
+                puts Outcome.all[21].outcome
                 end
             end
 
         end
     else
         if prompt.select(Outcome.all[13].outcome, dragon) == hide
-            p Outcome.all[14].outcome
+            puts Outcome.all[14].outcome
             if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                p Outcome.all[19].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[19].outcome
+                puts Outcome.all[21].outcome
             else
-            p Outcome.all[20].outcome
-            p Outcome.all[21].outcome
+            puts Outcome.all[20].outcome
+            puts Outcome.all[21].outcome
             end
         else
             if prompt.select(Outcome.all[15].outcome,fight1) == tail
-                p Outcome.all[16].outcome
+                puts Outcome.all[16].outcome
                 if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                    p Outcome.all[19].outcome
-                    p Outcome.all[21].outcome
+                    puts Outcome.all[19].outcome
+                    puts Outcome.all[21].outcome
                 else
-                p Outcome.all[20].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[20].outcome
+                puts Outcome.all[21].outcome
                 end
             
             
             else
-                p Outcome.all[17].outcome
+                puts Outcome.all[17].outcome
                 if prompt.select(Outcome.all[18].outcome,fight2) == sneak
-                    p Outcome.all[19].outcome
-                    p Outcome.all[21].outcome
+                    puts Outcome.all[19].outcome
+                    puts Outcome.all[21].outcome
                 else
-                p Outcome.all[20].outcome
-                p Outcome.all[21].outcome
+                puts Outcome.all[20].outcome
+                puts Outcome.all[21].outcome
                 end
             end
         end
